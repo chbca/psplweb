9c28bb6680a78865c9ad256fb521d892bca442c0
coolskull <824848997@qq.com>
2017-10-25 14:31:17 +0800

    添加图片压缩和usemin

--- landingPage/baking/GulpFile.js
+++ landingPage/baking/GulpFile.js
@@ -9,6 +9,10 @@ var gulp = require('gulp'),
     csso = require('gulp-csso'),
     imagemin = require('gulp-imagemin'),
     merge = require('merge-stream'),
+    usemin = require('gulp-usemin'),
+    uglify=require('gulp-uglify'),
+    rev=require('gulp-rev'),
+    imageminPngquant=require('imagemin-pngquant'),
     livereload = require('gulp-livereload');
 /*
     chrome浏览器上装livereload插件配合使用
@@ -17,7 +21,14 @@ var gulp = require('gulp'),
 gulp.task('clean', function () {
     return del(['dist']);
 });
-
+gulp.task('usemin', function () {
+    return gulp.src('workspace/*.html')
+        .pipe(usemin({
+            css: [rev()],
+            js: [uglify(), rev()],
+        }))
+        .pipe(gulp.dest('dist/'));
+});
 gulp.task('less', function () {
     gulp.src('workspace/less/*.less')
         .pipe(sourcemaps.init())
@@ -40,7 +51,7 @@ gulp.task('watch', function () {
 
 gulp.task('copy', function () {
     return gulp.src(
-        ['workspace/images/**', 'workspace/css/*', 'workspace/*.html'], {
+        ['workspace/images/**', 'workspace/css/*'], {
             base: 'workspace'   //如果设置为 base: 'js' 将只会复制 js目录下文件, 其他文件会忽略
         }
     ).pipe(gulp.dest('dist'));
@@ -66,14 +77,29 @@ gulp.task('sprite-icon', function () {
     // Pipe CSS stream through CSS optimizer and onto disk
     var cssStream = spriteData.css
         .pipe(csso())
-        .pipe(gulp.dest('workspace/css/'));
+        .pipe(gulp.dest('workspace/css'))
+        .pipe(gulp.dest('dist/css/'));
 
     // Return a merged stream to handle both `end` events
     return merge(imgStream, cssStream);
 });
+gulp.task('imagemin', function () {
+    return gulp.src('workspace/images/**')
+        .pipe(imagemin([imageminPngquant({quality: 40, speed: 1})]))
+        .pipe(gulp.dest('dist/images'));
+});
+gulp.task('dist-less', function () {
+    gulp.src('workspace/less/*.less')
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions', 'not ie <= 8'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css'))
+});
 
 
 gulp.task('default', sequence('clean', ['less', 'sprite-icon', 'watch']));
-gulp.task('build', sequence('clean', ['less', 'sprite-icon', 'copy']));
+gulp.task('build', sequence('clean', ['sprite-icon','imagemin','dist-less', 'usemin']));
 
 
